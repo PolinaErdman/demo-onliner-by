b9ae207b54169e7681bdb625bd75c882d991429b
palinaerdman <polinaerdman@gmail.com>
2024-10-31 19:54:39 +0300

    add LoginMessage

---
+++ src/test/java/by/onliner/LoginMessage.java
@@ -0,0 +1,7 @@
+package by.onliner;
+
+public class LoginMessage {
+    public static final String ENTER_A_NICKNAME_OR_EMAIL = "Укажите ник или e-mail";
+    public static final String ENTER_PASSWORD = "Укажите пароль";
+    public static final String INVALID_LOGIN_OR_PASSWORD = "Неверный логин или пароль";
+}
